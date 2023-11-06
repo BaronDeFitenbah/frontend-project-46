0ada22dbaaef86404681f251f6a3a7947e897826
BaronDeFitenbah <119754277+BaronDeFitenbah@users.noreply.github.com>
2023-11-06 20:14:55 +0400

    Исправление

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,27 @@
-import { readFileSync } from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+import getDiff from "../src/calcDiff.js";
 
-import parser from '../src/index.js';
+const fileExt = [".json", ".yml"];
 
 const getPath = (filename) => path.resolve('__fixtures__', filename);
 
-test('json files', test(`json`));
+const resultStylish = fs.readFileSync(
+	path.resolve(process.cwd(), "__fixtures__/result.txt"),
+	"utf-8"
+);
+const resultPlain = fs.readFileSync(
+	path.resolve(process.cwd(), "__fixtures__/result-plain.txt"),
+	"utf-8"
+);
 
-test('yaml files', test(`yml`));
-
-function test(format){
-  const filename1 = getPath(`file1.${format}`);
-  const filename2 = getPath(`file2.${format}`);
-  const resultname = getPath('result.txt');
-  const result = readFileSync(resultname, 'utf8');
-  expect(parser(filename1, filename2)).toBe(result);
-}
+test.each(fileExt)("testing different file options", (format) => {
+	const filename1 = getPath(`file1${format}`);
+	const filename2 = getPath(`file2${format}`);
+	const actual1 = getDiff(filename1, filename2, "stylish");
+	expect(actual1).toEqual(resultStylish);
+	const actual2 = getDiff(filename1, filename2, "plain");
+	expect(actual2).toEqual(resultPlain);
+	const actual4 = getDiff(filename1, filename2);
+	expect(actual4).toEqual(resultStylish);
+});
--- src/calcDiff.js
+++ src/calcDiff.js
@@ -3,32 +3,51 @@ import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
 import ast from './formatters/index.js';
+import getFormatting from './formatters/index.js'
 
-const compare = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keysSorted = _.sortBy(_.union(keys1, keys2));
+const compare = (obj1, obj2) => {
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  const result = keysSorted.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, type: 'nested', children: compare(data1[key], data2[key]) };
-    }
-    if (!Object.hasOwn(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return { key, type: 'deleted', value: data1[key] };
-    }
-    if (!_.isEqual(data1[key], data2[key])) {
+  const result = sortedKeys.map((key) => {
+    if (!_.has(obj1, key)) {
       return {
-        key, type: 'changed', value1: data1[key], value2: data2[key],
+        key,
+        value: obj2[key],
+        type: 'added',
+      }; 
+    }
+    if (!_.has(obj2, key)) {
+      return {
+        key,
+        value: obj1[key],
+        type: 'deleted',
       };
     }
-    return { key, type: 'unchanged', value: data1[key] };
+    if(_.isObject(obj1[key]) && _.isObject(obj2[key])){
+      return {
+        key,
+        type: 'nested',
+        children: compare(obj1[key], obj2[key]),
+      };
+    }
+    if (obj1[key] !== obj2[key]){
+      return {
+        key,
+        valueBefore: obj1[key],
+        valueAfter: obj2[key],
+        type: 'changed',
+      };
+    }
+    return {
+      key,
+      value: obj1[key],
+      type: 'unchanged'
+    };
   });
-
   return result;
-};
+}
 
 const getData = (filePath) => {
   const fileContent = fs.readFileSync(filePath, 'utf-8').trim();
