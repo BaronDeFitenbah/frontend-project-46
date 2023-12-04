d20dfbd6060c72d069546a44595d5c217b65d5ef
BaronDeFitenbah <119754277+BaronDeFitenbah@users.noreply.github.com>
2023-12-04 22:27:45 +0400

    исправил

--- code/index.js
+++ code/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import compare from './calcDiff.js';
 import ast from './formatters/index.js';
 import fs from 'fs';
+import parse from './parsers.js';
 
 const getPath = (filename) => path.resolve('__fixtures__', filename);
 
